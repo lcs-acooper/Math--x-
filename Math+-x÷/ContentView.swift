bf04d83bbc11929aa1930fe182e949ec4b9b64aa
AJ Cooper <acooper@lcs.on.ca>
2024-01-16 19:57:39 -0500

    Initial Commit

---
+++ Math+-x÷/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Math+-x÷
+//
+//  Created by  Adam-James  Cooper on 2024-01-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Math+-x÷/Math__x_App.swift
@@ -0,0 +1,17 @@
+//
+//  Math__x_App.swift
+//  Math+-x÷
+//
+//  Created by  Adam-James  Cooper on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct Math__x_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
