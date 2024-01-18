598029b244f8f1d9bf08147236e34ba7b59da6bf
AJ Cooper <acooper@lcs.on.ca>
2024-01-18 17:42:44 -0500

    Made tab views for all the seperate math types.

--- Math+-x÷/ContentView.swift
+++ Math+-x÷/ContentView.swift
@@ -9,16 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        TabView {
+                 // Addition tab
+                 NavigationView {
+                     VStack {
+                         Text("Addition")
+                         // Addition content
+                     }
+                 }
+                 .tabItem {
+                     Image(systemName: "plus.circle.fill")
+                     Text("Addition")
+                 }
+                 .tag(1)
 
+                 // Subtraction tab
+                 NavigationView {
+                     VStack {
+                         Text("Subtraction")
+                         //subtraction content
+                     }
+                 }
+                 .tabItem {
+                     Image(systemName: "minus.circle.fill")
+                     Text("Subtraction")
+                 }
+                 .tag(2)
+
+                 // Multiplication tab
+                 NavigationView {
+                     VStack {
+                         Text("Multiplication")
+                         //multiplication content
+                     }
+                 }
+                 .tabItem {
+                     Image(systemName: "multiply.circle.fill")
+                     Text("Multiplication")
+                 }
+                 .tag(3)
+
+                 // Division tab
+                 NavigationView {
+                     VStack {
+                         Text("Division")
+                         //division content
+                     }
+                 }
+                 .tabItem {
+                     Image(systemName: "divide.circle.fill")
+                     Text("Division")
+                 }
+                 .tag(4)
+             }
+         }
+     }
 #Preview {
     ContentView()
 }
+
