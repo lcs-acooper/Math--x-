5fb5d21156bb6fe6090d16887c21779654595994
AJ Cooper <acooper@lcs.on.ca>
2024-01-21 14:54:31 -0500

    Added all the stored properties as well as the computed properties for addtion and subtraction. Started the tab view for addition as well as subtraction

--- Math+-x÷/ContentView.swift
+++ Math+-x÷/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: Stored Properties Addition
+    @State var base1: Int = 1
+    @State var baseAdd: Int = 1
+    //MARK: Stored Properties Substraction
+    @State var base2: Int = 1
+    @State var baseSub: Int = 1
+    //MARK: Stored Properties Multiplication
+    @State var base3: Int = 1
+    //MARK: Stored Properties Division
+    @State var base4: Int = 1
+    
+    //MARK: Computed potatos. Properties
+    //Addition
+    var addition: Int {
+        return base1 + baseAdd
+    }
+    //Subtraction
+    var subtaction: Int {
+        return base2 - baseSub
+    }
+    
+    
     var body: some View {
         TabView {
                  // Addition tab
                  NavigationView {
+                     //Vertical stack
                      VStack {
-                         Text("Addition")
-                         // Addition content
+                         //First Number
+                         HStack {
+                            Spacer()
+                             Text("\(base1)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         //Stepper First Number
+                         HStack {
+                             Stepper(value: $base1, label: {
+                                 Text("Select first number")
+                             })
+                             .padding()
+                         }
+                         //Second Number
+                         HStack {
+                             Image(systemName: "plus")
+                                 .font(.system(size: 60))
+                             Spacer()
+                             Text("\(baseAdd)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         //Stepper Second Number
+                         HStack {
+                             Stepper(value: $baseAdd, label: {
+                                 Text("Select first number")
+                             })
+                             .padding()
+                         }
+                         //Answer
+                         HStack {
+                            Spacer()
+                             Text("\(addition)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         
+                        
                      }
+                     
                  }
                  .tabItem {
                      Image(systemName: "plus.circle.fill")
@@ -26,8 +88,43 @@ struct ContentView: View {
                  // Subtraction tab
                  NavigationView {
                      VStack {
-                         Text("Subtraction")
-                         //subtraction content
+                         //First Number
+                         HStack {
+                            Spacer()
+                             Text("\(base2)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         //Stepper First Number
+                         HStack {
+                             Stepper(value: $base2, label: {
+                                 Text("Select first number")
+                             })
+                             .padding()
+                         }
+                         //Second Number
+                         HStack {
+                             Spacer()
+                             Text("\(baseSub)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         //Stepper Second Number
+                         HStack {
+                             Stepper(value: $baseSub, label: {
+                                 Text("Select first number")
+                             })
+                             .padding()
+                         }
+                         //Answer
+                         HStack {
+                            Spacer()
+                             Text("\(subtaction)")
+                                 .font(.system(size: 72))
+                                 .padding()
+                         }
+                         
+                        
                      }
                  }
                  .tabItem {
